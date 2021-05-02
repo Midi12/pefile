cceb8f4039dc4f8635d7470b2b528950e41db30c
Midi12 <Midi12@users.noreply.github.com>
2021-05-02 14:49:14 +0200

    feat(pefile): added section parsing

--- example/pefile_example.dart
+++ example/pefile_example.dart
@@ -1,10 +1,18 @@
-import 'dart:io';
-
 import 'package:pefile/pefile.dart' as pefile;
 
-Future<void> main() async {
-  var pe64 = pefile.parse('C:\\Windows\\System32\\notepad.exe') as pefile.PeFile64;
+void parse<T extends pefile.PeFileBase>(String path) {
+  print('parsing $path');
+  var pe = pefile.parse(path) as T;
 
-  var dos_hdr = pe64.dos_header;
+  var dos_hdr = pe.dos_header;
   print(dos_hdr.e_magic);
+  print(pe.header_data.entry_point_rva.toRadixString(16));
+  for(var section in pe.sections) {
+    print('Section ${section.name}\taddress ${section.virtual_address.toRadixString(16)}h\tsize ${section.virtual_size.toRadixString(16)}h');
+  }
+}
+
+Future<void> main() async {
+  parse<pefile.PeFile64>('C:\\Windows\\System32\\notepad.exe');
+  parse<pefile.PeFile32>('C:\\Windows\\SysWOW64\\notepad.exe');
 }
--- lib/src/pefile_base.dart
+++ lib/src/pefile_base.dart
@@ -8,6 +8,8 @@ import 'idisposable.dart';
 import './structs/image_dos_header.dart';
 import './structs/image_nt_headers.dart';
 import './structs/image_optional_header.dart';
+import './structs/image_section_header.dart';
+import './structs/image_file_header.dart';
 
 class PeFileException implements Exception {
   final String _message;
@@ -20,10 +22,61 @@ class PeFileException implements Exception {
   String toString() => 'PeFileException: $_message';
 }
 
+class HeaderData {
+  final int _image_base;
+  final int _image_size;
+  final int _header_size;
+  final int _entry_point_rva;
+  final int _subsystem;
+  final int _dll_characteristics;
+
+  HeaderData(this._image_base, this._image_size, this._header_size, this._entry_point_rva, this._subsystem, this._dll_characteristics);
+
+  int get image_base => _image_base;
+  int get image_size => _image_size;
+  int get header_size => _header_size;
+  int get entry_point_rva => _entry_point_rva;
+  int get subsystem => _subsystem;
+  int get dll_characteristics => _dll_characteristics;
+}
+
+class Section {
+  final String _name;
+  final int _virtual_size;
+  final int _virtual_address;
+  final int _size_of_raw_data;
+  final int _pointer_to_raw_data;
+  final int _characteristics;
+
+  Section(this._name, this._virtual_size, this._virtual_address, this._size_of_raw_data, this._pointer_to_raw_data, this._characteristics);
+
+  String get name => _name;
+  int get virtual_size => _virtual_size;
+  int get virtual_address => _virtual_address;
+  int get size_of_raw_data => _size_of_raw_data;
+  int get pointer_to_raw_data => _pointer_to_raw_data;
+  int get characteristics => _characteristics;
+
+  bool get isCodeSection => (_characteristics & IMAGE_SCN_CNT_CODE) == 1;
+  bool get isDataSection => (_characteristics & IMAGE_SCN_CNT_INITIALIZED_DATA) == 1 || (_characteristics & IMAGE_SCN_CNT_UNINITIALIZED_DATA) == 1;
+}
+
+class Import {
+
+}
+
+class Export {
+
+}
+
 class PeFileBase implements IDisposable {
 
   late Pointer<Uint8> _buffer;
   late int _size;
+
+  final List<Section> _sections = <Section>[];
+  final List<Export> _exports = <Export>[];
+  final List<Import> _imports = <Import>[];
 
   factory PeFileBase.fromPath(String path) {
     var file = File(path);
@@ -67,7 +120,29 @@ class PeFileBase implements IDisposable {
   }
 
   Struct get nt_headers => throw UnimplementedError();
+  HeaderData get header_data => throw UnimplementedError();
 
+  List<Section> get sections => _sections;
+  List<Export> get exports => _exports;
+  List<Import> get imports => _imports;
+}
+
+List<Section> _parseSectionImpl(Pointer<Uint8> buffer, int nt_hdr_offset, int nt_hdr_size, int num_sections, List<Section> sections) {
+  var sections_it = buffer.elementAt(nt_hdr_offset + sizeOf<IMAGE_FILE_HEADER>() + 4 + nt_hdr_size).cast<IMAGE_SECTION_HEADER>();
+
+  for (var i = 0; i < num_sections; i++) {
+    var section = sections_it.elementAt(i);
+    sections.add(Section(
+      section.szName,
+      section.ref.VirtualSize,
+      section.ref.VirtualAddress,
+      section.ref.SizeOfRawData,
+      section.ref.PointerToRawData,
+      section.ref.Characteristics
+    ));
+  }
+
+  return sections;
 }
 
 class PeFile32 extends PeFileBase {
@@ -83,6 +158,29 @@ class PeFile32 extends PeFileBase {
 
     return pNtHdrs.ref;
   }
+
+  @override
+  HeaderData get header_data {
+    var opt_hdr = nt_headers.OptionalHeader;
+
+    return HeaderData(
+      opt_hdr.ImageBase,
+      opt_hdr.SizeOfImage,
+      opt_hdr.SizeOfHeaders,
+      opt_hdr.AddressOfEntryPoint,
+      opt_hdr.Subsystem,
+      opt_hdr.DllCharacteristics
+    );
+  }
+
+  @override
+  List<Section> get sections => _sections.isEmpty ? _parseSectionImpl(_buffer, dos_header.e_lfanew, nt_headers.FileHeader.SizeOfOptionalHeader, nt_headers.FileHeader.NumberOfSections, _sections) : _sections;
+
+  @override
+  List<Export> get exports => throw UnimplementedError();
+
+  @override
+  List<Import> get imports => throw UnimplementedError();
 }
 
 class PeFile64 extends PeFileBase {
@@ -98,6 +196,29 @@ class PeFile64 extends PeFileBase {
 
     return pNtHdrs.ref;
   }
+
+  @override
+  HeaderData get header_data {
+    var opt_hdr = nt_headers.OptionalHeader;
+
+    return HeaderData(
+      opt_hdr.ImageBase,
+      opt_hdr.SizeOfImage,
+      opt_hdr.SizeOfHeaders,
+      opt_hdr.AddressOfEntryPoint,
+      opt_hdr.Subsystem,
+      opt_hdr.DllCharacteristics
+    );
+  }
+
+  @override
+  List<Section> get sections => _sections.isEmpty ? _parseSectionImpl(_buffer, dos_header.e_lfanew, nt_headers.FileHeader.SizeOfOptionalHeader, nt_headers.FileHeader.NumberOfSections, _sections) : _sections;
+
+  @override
+  List<Export> get exports => throw UnimplementedError();
+
+  @override
+ List<Import> get imports => throw UnimplementedError();
 }
 
 PeFileBase parse(dynamic file) {
